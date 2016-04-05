2fab9a115a219c3f8a7d894c99b03832e5319df9
Lefteris Karapetsas <lefteris@refu.co>
2016-04-05 09:56:09 +0200

    parameterizable solc and code.js in gitignore

--- deploy/compile.py
+++ deploy/compile.py
@@ -1,7 +1,17 @@
+#!/usr/bin/python2
+
 import json
 import subprocess
+import argparse
+import os
+import inspect
 
 contracts_dir = "../"
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe()))
+)
+os.sys.path.insert(0, os.path.dirname(currentdir))
+from tests.utils import determine_binary
 
 
 def compile_contract(solc, contract_path):
@@ -17,14 +27,21 @@ def compile_contract(solc, contract_path):
 
 
 if __name__ == "__main__":
-    comp = compile_contract("/usr/bin/solc", "../DAO.sol")
+    p = argparse.ArgumentParser(description='DAO deployment script')
+    p.add_argument(
+        '--solc',
+        help='Full path to the solc binary to use'
+    )
+    args = p.parse_args()
+    solc = determine_binary(args.solc, 'solc')
+    comp = compile_contract(solc, "../DAO.sol")
 
     with open("code.js", "w") as f:
-        f.write("dao_abi = {}\n".format(comp['contracts']['DAO']['abi']))
-        f.write("dao_bin = '{}'\n".format(comp['contracts']['DAO']['bin']))
-        f.write("creator_abi = {}\n".format(
+        f.write("dao_abi = {};\n".format(comp['contracts']['DAO']['abi']))
+        f.write("dao_bin = '{}';\n".format(comp['contracts']['DAO']['bin']))
+        f.write("creator_abi = {};\n".format(
           comp['contracts']['DAO_Creator']['abi'])
         )
-        f.write("creator_bin = '{}'\n".format(
+        f.write("creator_bin = '{}';\n".format(
           comp['contracts']['DAO_Creator']['bin'])
         )
