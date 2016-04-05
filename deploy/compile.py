6b2b29601e20df7de0e984ae3c428fa66f823c4c
Lefteris Karapetsas <lefteris@refu.co>
2016-04-05 09:41:27 +0200

    compile.py is now executable plus styling

--- deploy/compile.py
+++ deploy/compile.py
@@ -1,29 +1,30 @@
-
-
-import os 
-import pdb
 import json
 import subprocess
 
-contracts_dir= "../"
+contracts_dir = "../"
+
 
 def compile_contract(solc, contract_path):
-  print("    Compiling {}...".format(contract_path))
-  data = subprocess.check_output([
-  solc,
-  contract_path,
-  "--optimize",
-  "--combined-json",
-  "abi,bin"
-  ])
-  return json.loads(data)
-
-comp =compile_contract("/usr/bin/solc", "../DAO.sol" )
-
-with open("code.js", "w") as f:
-  f.write("dao_abi =" + comp['contracts']['DAO']['abi'])
-  f.write("dao_bin = '" + comp['contracts']['DAO']['bin']+ "'\n")
-  f.write("creator_abi =" + comp['contracts']['DAO_Creator']['abi'])
-  f.write("creator_bin = '" + comp['contracts']['DAO_Creator']['bin'] + "'\n")
+    print("    Compiling {}...".format(contract_path))
+    data = subprocess.check_output([
+      solc,
+      contract_path,
+      "--optimize",
+      "--combined-json",
+      "abi,bin"
+    ])
+    return json.loads(data)
 
 
+if __name__ == "__main__":
+    comp = compile_contract("/usr/bin/solc", "../DAO.sol")
+
+    with open("code.js", "w") as f:
+        f.write("dao_abi = {}\n".format(comp['contracts']['DAO']['abi']))
+        f.write("dao_bin = '{}'\n".format(comp['contracts']['DAO']['bin']))
+        f.write("creator_abi = {}\n".format(
+          comp['contracts']['DAO_Creator']['abi'])
+        )
+        f.write("creator_bin = '{}'\n".format(
+          comp['contracts']['DAO_Creator']['bin'])
+        )
